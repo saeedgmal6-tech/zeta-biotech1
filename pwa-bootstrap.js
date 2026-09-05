73b22759403e18fda64d384bfaf78ca0b00575cd
saeedgmal6-tech <saeedgmal6@gmail.com>
2026-09-05 19:58:18 +0300

    Add non-invasive PWA bootstrap

---
+++ pwa-bootstrap.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function(){
+      navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).catch(function(){});
+    });
+  }
+})();
